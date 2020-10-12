ba03b95286a4c25ef1129b1b2b383a74c0695131
freetimezp <kraftukrnet@ukr.net>
2020-10-12 22:01:23 +0300

    React-js. Categories component. functional component. state setState

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,62 +1,75 @@
 import React from 'react';
 
-class Categories extends React.Component {
-    state = {
-        activeItem: null
-    };
+const Categories = ({items, onClickItem}) => {
+    const [activeItem, setActiveItem] = React.useState(null);
 
-    onSelectItem = (index) => {
-        this.setState({
-            activeItem: index
-        });
-    };
-
-    render() {
-        const {items, onClickItem} = this.props;
-        return (
-            <div className="categories">
-                <ul>
-                    <li
-                        onClick={() => this.onSelectItem(null)}
-                        className={this.state.activeItem === null ? 'active' : ''}
-                    >
-                        Все
-                    </li>
-                    {
-                        items.map((item, index) =>
-                            <li
-                                className={this.state.activeItem === index ? 'active' : ''}
-                                onClick={() => this.onSelectItem(index)}
-                                key={`${item}_${index}`}
-                            >
-                                {item}
-                            </li>
-                        )
-                    }
-                </ul>
-            </div>
-        );
+    const onSelectItem = (index) => {
+      setActiveItem(index);
     }
+
+    return (
+        <div className="categories">
+            <ul>
+                <li
+                    className={activeItem === null ? 'active' : ''}
+                    onClick={() => setActiveItem(null)}
+                >
+                    Все
+                </li>
+                {
+                    items.map((item, index) =>
+                        <li
+                            className={activeItem === index ? 'active' : ''}
+                            onClick={() => onSelectItem(index)}
+                            key={`${item}_${index}`}
+                        >
+                            {item}
+                        </li>
+                    )
+                }
+            </ul>
+        </div>
+    );
 }
 
-// const Categories = ({items, onClickItem}) => {
-//     return (
-//         <div className="categories">
-//             <ul>
-//                 <li className="active">Все</li>
-//                 {
-//                     items.map((item, index) =>
-//                         <li
-//                             onClick={onClickItem}
-//                             key={`${item}_${index}`}
-//                         >
-//                             {item}
-//                         </li>
-//                     )
-//                 }
-//             </ul>
-//         </div>
-//     );
+// class Categories extends React.Component {
+//     state = {
+//         activeItem: null
+//     };
+//
+//     onSelectItem = (index) => {
+//         this.setState({
+//             activeItem: index
+//         });
+//     };
+//
+//     render() {
+//         const {items, onClickItem} = this.props;
+//         return (
+//             <div className="categories">
+//                 <ul>
+//                     <li
+//                         onClick={() => this.onSelectItem(null)}
+//                         className={this.state.activeItem === null ? 'active' : ''}
+//                     >
+//                         Все
+//                     </li>
+//                     {
+//                         items.map((item, index) =>
+//                             <li
+//                                 className={this.state.activeItem === index ? 'active' : ''}
+//                                 onClick={() => this.onSelectItem(index)}
+//                                 key={`${item}_${index}`}
+//                             >
+//                                 {item}
+//                             </li>
+//                         )
+//                     }
+//                 </ul>
+//             </div>
+//         );
+//     }
 // }
+
 
 export default Categories;
