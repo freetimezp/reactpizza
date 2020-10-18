a3045c3bd2ed10618187bc941986a4e1f7076d38
freetimezp <kraftukrnet@ukr.net>
2020-10-18 19:37:32 +0300

    React-js. PizzaBlock component. useState. fetch. add active or disabled class to required item.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Route} from 'react-router-dom';
 
 import {Header, Home, Cart} from "./components/components.js";
 
+
 function App() {
+    const[pizzas, setPizzas] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/db.json')
+            .then((response) => response.json())
+            .then((json) => {
+                setPizzas(json.pizzas);
+            });
+    }, []);
+
     return (
         <div className="wrapper">
             <Header />
             <div className="content">
-                <Route exact path='/' component={Home} />
-                <Route exact path='/cart' component={Cart} />
+                <Route exact path='/' render={() => <Home items={pizzas} />} />
+                <Route exact path='/cart' render={() => <Cart />} />
             </div>
         </div>
     );
--- src/components/components.js
+++ src/components/components.js
@@ -2,8 +2,9 @@ import Header from './Header';
 import Button from './Button';
 import Categories from "./Categories";
 import SortPopup from "./SortPopup";
+import PizzaBlock from "./PizzaBlock";
 
 import Home from "../pages/Home";
 import Cart from "../pages/Cart";
 
-export {Header, Button, Categories, SortPopup, Home, Cart};
+export {Header, Button, Categories, SortPopup, Home, Cart, PizzaBlock};
